5656b599955fc7746a8c42fa19834c70c7e1790e
Elyess Eleuch <ely.369@outlook.com>
2022-05-12 12:21:16 +0200

    feat(Sound): add sound when we pass a checkpoint

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -6,6 +6,14 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
     public static event Action onCollisionWithCheckpoint;
+    public AudioClip checkpointPassed; 
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Env")
@@ -17,6 +25,7 @@ public class Collision : MonoBehaviour
         else if (other.gameObject.tag == "Checkpoint")
         {
             Debug.Log("Passed through a checkpoint");
+            audioSource.PlayOneShot(checkpointPassed);
             onCollisionWithCheckpoint?.Invoke();
         }
     }
