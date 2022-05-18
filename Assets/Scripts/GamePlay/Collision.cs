7c511f995290177bb6b1fc387d5bc8fd65c6dc8a
Elyess Eleuch <ely.369@outlook.com>
2022-05-18 16:59:23 +0200

    feat(Camera): add camera shake

--- Assets/Scripts/GamePlay/Collision.cs
+++ Assets/Scripts/GamePlay/Collision.cs
@@ -8,10 +8,23 @@ public class Collision : MonoBehaviour
     public static event Action onCollisionWithCheckpoint;
     public AudioClip checkpointPassed; 
     AudioSource audioSource;
+    public CamerShake cameraShake;
 
     void Awake()
     {
         audioSource = gameObject.GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, 50))
+        {
+            Debug.Log("There is something in front of the object");
+        }
+        else
+        {
+            Debug.Log("There is nothing");
+        }
     }
     
     private void OnTriggerEnter(Collider other)
@@ -25,6 +38,7 @@ public class Collision : MonoBehaviour
         else if (other.gameObject.tag == "Checkpoint")
         {
             Debug.Log("Passed through a checkpoint");
+            StartCoroutine(cameraShake.Shake(.06f, .6f));
             audioSource.PlayOneShot(checkpointPassed);
             onCollisionWithCheckpoint?.Invoke();
         }
