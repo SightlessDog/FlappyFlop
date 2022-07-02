5efccf1e27a61aa68f6cbeed1d501e4b7ed72dad
Linh Pham <bptl1603@gmail.com>
2022-07-02 14:56:30 +0200

    adjust lighting

---
+++ Assets/Scripts/Utils/CharacterFollow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class CharacterFollow : MonoBehaviour
+{
+    public GameObject mainCharacter;
+    
+    void Update()
+    {
+        transform.position = mainCharacter.transform.position;
+    }
+}
