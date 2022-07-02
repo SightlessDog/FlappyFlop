48112241596572a596edccc37a7b4acb4cda0788
Linh Pham <bptl1603@gmail.com>
2022-07-02 05:17:08 +0200

    documentation

--- Assets/Scripts/Generator/PolygonMeshGenerator.cs
+++ Assets/Scripts/Generator/PolygonMeshGenerator.cs
@@ -8,48 +8,61 @@ using UnityEngine;
 public class PolygonMeshGenerator : MonoBehaviour
 {
     Mesh mesh;
-    public Vector3[] polygonPoints;
-    public int[] polygonTriangles;
+    public Vector3[] vertices;
+    public int[] triangles;
 
     public int polygonSides;
-    public float polygonRadius;
-    public float centerRadius;
+    public float polygonOuterRadius;
+    public float centerInnerRadius;
 
     void Start()
     {
         mesh = new Mesh();
         this.GetComponent<MeshFilter>().mesh = mesh;
+        //Modify number of polygon sides through the time
         InvokeRepeating("TransformPolygon", 1f, 1f);
     }
 
     void Update()
     {
-        DrawHollow(polygonSides, polygonRadius, centerRadius);
+        DrawHollow(polygonSides, polygonOuterRadius, centerInnerRadius);
+        UpdateMesh();
     }
 
-    void DrawHollow(int sides, float outerRadius, float innerRadius)
+    /// <summary>
+    /// Draw the hollow polygon based on the given number of
+    /// sides, outerRadius and innerRadius
+    /// </summary>
+    /// <param name="sides"></param>
+    /// <param name="outerRadius"></param>
+    /// <param name="innerRadius"></param>
+    private void DrawHollow(int sides, float outerRadius, float innerRadius)
     {
+        //Defining List of vertices
         List<Vector3> pointsList = new List<Vector3>();
         List<Vector3> outerPoints = GetCircumferencePoints(sides, outerRadius);
         pointsList.AddRange(outerPoints);
         List<Vector3> innerPoints = GetCircumferencePoints(sides, innerRadius);
         pointsList.AddRange(innerPoints);
         
-        polygonPoints = pointsList.ToArray();
-
-        polygonTriangles = DrawHollowTrianges(polygonPoints);
-
-        mesh.Clear();
-        mesh.vertices = polygonPoints;
-        mesh.triangles = polygonTriangles;
+        vertices = pointsList.ToArray();
+        
+        //Draw Triangles based on the vertices
+        triangles = DrawHollowTriangles(vertices);
     }
 
-    int[] DrawHollowTrianges(Vector3[] points)
+    /// <summary>
+    /// List of triangles to create the hollow polygon based on the given sorted vertices
+    /// </summary>
+    /// <param name="points"></param>
+    /// <returns></returns>
+    private int[] DrawHollowTriangles(Vector3[] points)
     {
         int sides = points.Length / 2;
         int triangleAmount = sides * 2;
-
         List<int> newTriangles = new List<int>();
+        
+        //Defining triangles
         for (int i = 0; i < sides; i++)
         {
             int outerIndex = i;
@@ -67,28 +80,57 @@ public class PolygonMeshGenerator : MonoBehaviour
         return newTriangles.ToArray();
     }
 
-    List<Vector3> GetCircumferencePoints(int sides, float radius)
+    /// <summary>
+    /// Return a list of circumference vertices of the polygon based on
+    /// the number of points and radius we want to have
+    /// </summary>
+    /// <param name="pointNum"></param>
+    /// <param name="radius"></param>
+    /// <returns></returns>
+    private List<Vector3> GetCircumferencePoints(int pointNum, float radius)
     {
         List<Vector3> points = new List<Vector3>();
-        float circumferenceProfessPerStep = (float) 1 / sides;
+        
+        //distance between the points
+        float circumferenceProgressPerStep = (float) 1 / pointNum;
+        //TAU time constant
         float TAU = 2 * Mathf.PI;
-        float radianProgressPerStep = circumferenceProfessPerStep * TAU;
+        //radian value between 2 points
+        float radianProgressPerStep = circumferenceProgressPerStep * TAU;
 
-        for (int i = 0; i < sides; i++)
+        //add calculated points to the list
+        for (int i = 0; i < pointNum; i++)
         {
+            //radian between first point and current point
             float currentRadian = radianProgressPerStep * i;
+            //calculate the point on a xy-axis
             points.Add(new Vector3(Mathf.Cos(currentRadian) * radius, Mathf.Sin(currentRadian) * radius, 0));
         }
 
         return points;
     }
 
-    void TransformPolygon()
+    /// <summary>
+    /// Adjusting the sides of polygon through the time
+    /// </summary>
+    private void TransformPolygon()
     {
         polygonSides++;
         if (polygonSides >= 10)
         {
             polygonSides = 3;
         }
+    }
+
+    /// <summary>
+    /// Update the mesh attributes
+    /// </summary>
+    private void UpdateMesh()
+    {
+        mesh.Clear();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
     }
 }
